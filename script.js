890aa3064d0039019cbc6ce2fc7f162e11982bda
Chaitanya Keshniya <chaitanyakeshniya1404@gmail.com>
2025-10-22 08:32:51 +0530

    feat: render fetched movies on page

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ document.addEventListener('DOMContentLoaded', () => {
     const searchForm = document.getElementById('search-form');
     const searchInput = document.getElementById('search-input');  
     const carouselSlides = document.querySelectorAll('.carousel-slide');
+    const contentArea = document.getElementById('content-area');
 
     let bannerMovies = [];
     let watchList = [];
@@ -49,14 +50,14 @@ document.addEventListener('DOMContentLoaded', () => {
         if (!query) return;
 
         try {
-            const response = await fetch(`https://www.omdbapi.com/?s=${query}&apikey=${OMDb_API_KEY}`);
+            const response = await fetch(`https://www.omdbapi.com/?s=${encodeURIComponent(query)}&apikey=${OMDb_API_KEY}`);
             const data = await response.json();
-            console.log(data);
+            // console.log(data);
             if (data.Response === "True") {
-                const movieDetailsPromises = data.Search.map(movie => fetch(`https://www.omdbapi.com/?i=${movie.imdbID}&apikey=${OMDb_API_KEY}`).then(res => res.json()));
+                const movieDetailsPromises = data.Search.slice(0, 8).map(movie => fetch(`https://www.omdbapi.com/?i=${movie.imdbID}&apikey=${OMDb_API_KEY}`).then(res => res.json()));
                 const movieWithDetails = await Promise.all(movieDetailsPromises);
-                console.log(movieWithDetails); 
-                
+                // console.log(movieWithDetails); 
+                renderMovies(movieWithDetails); 
             } else {
                 console.log('No movies found for the search query.');
             }
@@ -66,7 +67,35 @@ document.addEventListener('DOMContentLoaded', () => {
     }
     searchForm.addEventListener('submit', handleSearch);
 
-   
+   function getMovieHTML(movie) {
+        return `
+            <div class="movie-card">
+                <div class="movie-poster movie-card-clickable" data-imdb-id="${movie.imdbID}">
+                    <img src="${movie.Poster !== 'N/A' ? movie.Poster : './placeholder.svg'}" alt="${movie.Title}" class="movie-poster"/>
+                </div>
+                <div class="movie-details">
+                    <div>
+                        <div class=movie-title-rating>
+                            <h3 class="movie-title movie-card-clickable" data-imdb-id="${movie.imdbID}">${movie.Title} <span class="movie-year">(${movie.Year})</span></h3>
+                        </div>
+                        <div class="movie-rating">
+                            <span>⭐ ${movie.imdbRating || 'N/A'}</span>
+                        </div>
+                    </div>   
+                    <div class="movie-meta">
+                        <span>${movie.Runtime || 'N/A'}</span>
+                        <span>${movie.Genre || 'N/A'}</span>
+                    </div> 
+                    <p class="movie-plot">${movie.Plot !== 'N/A' ? movie.Plot.substring(0, 150) + '...' : ""}</p>
+                </div>
+            </div>
+        `
+   }
+
+   function renderMovies(movies) {
+        const moviesHTML = movies.map(movie => getMovieHTML(movie)).join('');
+        contentArea.innerHTML = `<div class="movies-grid">${moviesHTML}</div>`;
+   }
 
 
  
